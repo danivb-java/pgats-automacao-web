90647f2f589a3d67add48598909a9cbd84294d52
unknown <you@example.com>
2025-10-17 21:42:00 -0300

    adicionando exercicios da disciplica

---
+++ cypress/e2e/drag-and-drop-andwindows.cy.js
@@ -0,0 +1,27 @@
+
+describe(`Drag ans Drop ans Windows`, () =>  {
+
+    it('Multiple Wondows', () => {
+        cy.visit('https://the-internet.herokuapp.com/windows')
+
+        cy.contains('Click Here')
+        //executa funcoes no navegador (https://docs.cypress.io/api/commands/invoke)
+        .invoke('removeAttr', 'target')
+        .click()
+
+        cy.get('h3').should('have.next', 'New Window')
+
+        cy.go('back')
+        cy.get('h3').should('have.text', 'Opening a new window')
+    });
+
+    it(`Drag and Drop`, () => {
+        cy.visit(`https://the-internet.herokuapp.com/drag_and_drop`)
+
+        const dataTransfer = new DataTransfer
+        cy.contains(`A`).trigger('dragstart', { dataTransfer })
+        cy.contains(`B`).trigger('drop', { dataTransfer })
+
+
+    })
+});
