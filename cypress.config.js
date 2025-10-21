bdede9fe54ce3738cbf845ef22894f74326106b6
unknown <you@example.com>
2025-10-21 20:19:19 -0300

    artefatos

--- cypress.config.js
+++ cypress.config.js
@@ -7,6 +7,12 @@ module.exports = defineConfig({
   },
   e2e: {
     reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
